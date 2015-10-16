fdbe7cc8669ff99095b2beaa94222fe996d4c382
Chelsea <chelseadeonne@gmail.com>
2015-10-16 02:29:29 -0700

    added sound effects to click methods

--- script.js
+++ script.js
@@ -2,33 +2,64 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 2;
 var match_counter = 0;
+var music_theme = false;
 $(document).ready(function(){
     $(".card").click(function(){
+        card_flip.play();
         var current_card = $(this).find(".front").attr("data-card");
-        $(this).find(".back").fadeOut(500);
         if (first_card_clicked == null) {
+            $(this).find(".back").html("<img src='images/bullet.png'>").fadeOut(500);
             first_card_clicked = current_card;
             console.log("The first card you clicked is: " + first_card_clicked);
         }else {
+            $(this).find(".back").html("<img src='images/bullet.png'>").fadeOut(500);
             second_card_clicked = current_card;
             console.log("second card was clicked: " + second_card_clicked);
             if (first_card_clicked === second_card_clicked) {
+                match.play();
                 console.log("match!");
                 first_card_clicked = null;
                 second_card_clicked = null;
                 match_counter++;
                 if (match_counter >= total_possible_matches) {
-                    $("#game-area").html("<h2>You Won!</h2>");
+                    music_theme = false;
+                    $("#game-area").delay(500).fadeIn(200,function(){
+                        $("#game-area").html("<h1>Winner!</h1>");
+                    });
+
+                    if (music_theme === false) {
+                        theme_music.pause();
+                        $(".music").hide();
+                    }
+                    game_won.play();
                     console.log("You Won!");
                 }
             }else {
+                no_match.play();
                 console.log("no match");
-                $(".back").fadeIn(2000);
+                $(".back").delay(1000).fadeIn(200,function(){
+                    $(".back").html("<img src='images/skynet.jpg'>");
+                });
                 first_card_clicked = null;
                 second_card_clicked = null;
             }
         }
-
     });
-
+    $(".music").click(function(){
+        if (music_theme === false) {
+            music_theme = true;
+            theme_music.play();
+            $(".music").html("Stop Music")
+        }else {
+            theme_music.pause();
+            $(".music").html("Play Music");
+            music_theme = false;
+        }
+    });
+    $(".reset").click(function(){
+        reboot.play();
+        first_card_clicked = null;
+        second_card_clicked = null;
+        match_counter = 0;
+    });
 });
