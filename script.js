933307b529c0cbf5bb003dba8875f0227429c962
Chelsea <chelseadeonne@gmail.com>
2015-10-20 14:15:55 -0700

    test 2 - issues with reset function

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ var card2;
 $(document).ready(function(){
     function display_stats() {
         $(".games-played .value").text(games_played);
-        $(".attempts .value").text(attempts);
+        $(".attempts .value").text(attempts + matches);
         $(".accuracy .value").html(calculate_accuracy()).append("&#37;");
     }
     function calculate_accuracy() {
         if (matches === 0) {
             return 0;
         }else {
-            return Math.floor(matches/attempts * 100);
+            return Math.floor(matches/(attempts + matches) * 100);
         }
     }
     function reset_stats() {
@@ -32,28 +32,42 @@ $(document).ready(function(){
         attempts = 0;
         display_stats();
     }
-    $(".card").on('click', function(){
-        card_flip.play();
+    function reset() {
+        $(".music").show();
+        reboot.play();
+        games_played++;
+        reset_stats();
+        display_stats();
+        first_card_clicked = null;
+        second_card_clicked = null;
+        $(".card").on('click', flip_card).find(".back").html("<img src='images/skynet.jpg'>").fadeIn();
+        $(".title").html(game_title);
+    }
+    function flip_card() {
         var current_card = $(this).find(".front").attr("data-card");
+        console.log(current_card);
         if (first_card_clicked == null) {
             card1 = $(this);
             card1.find(".back").html("<img src='images/metal_bullet_hole.png'>").fadeOut(1000);
+            card_flip.play();
             first_card_clicked = current_card;
             console.log("The first card you clicked is: " + first_card_clicked);
         }else {
             card2 = $(this);
             card2.find(".back").html("<img src='images/metal_bullet_hole.png'>").fadeOut(1000);
             second_card_clicked = current_card;
-            attempts++;
             console.log("second card was clicked: " + second_card_clicked);
             if (first_card_clicked === second_card_clicked) {
                 match.play();
                 console.log("match!");
+                $(card1).off("click");
+                $(card2).off("click");
                 first_card_clicked = null;
                 second_card_clicked = null;
                 matches++;
                 if (matches >= total_possible_matches) {
                     music_theme = false;
+                    $(".reset").on('click', reset);
                     $("#game-area").delay(500).fadeIn(200,function(){
                         $(".title").html(winner).append(again, yes, no);
                         $(".affirmative").on('click', function(){
@@ -61,9 +75,9 @@ $(document).ready(function(){
                             games_played++;
                             reset_stats();
                             display_stats();
+                            $(".card").on('click', flip_card).find(".back").html("<img src='images/skynet.jpg'>").fadeIn();
                             first_card_clicked = null;
                             second_card_clicked = null;
-                            $(".card").find(".back").html("<img src='images/skynet.jpg'>").fadeIn();
                             $(".title").html(game_title);
                             console.log("affirmative");
                         });
@@ -78,10 +92,12 @@ $(document).ready(function(){
                         $(".music").html("Play Music").hide();
                     }
                     game_won.play();
+                    $(".card").off('click', flip_card);
                     console.log("You Won!");
                 }
             }else {
                 no_match.play();
+                attempts++;
                 console.log("no match");
                 $(first_card_clicked).find(".back").fadeIn().html("<img src='images/skynet.jpg'>");
                 $(second_card_clicked).find(".card").find(".back").fadeIn().html("<img src='images/skynet.jpg'>");
@@ -96,7 +112,8 @@ $(document).ready(function(){
             }
         }
         display_stats();
-    });
+    }
+    $(".card").on('click', flip_card);
     $(".music").on('click', function(){
         if (music_theme === false) {
             music_theme = true;
@@ -107,17 +124,5 @@ $(document).ready(function(){
             $(".music").html("Play Music");
             music_theme = false;
         }
-    });
-    $(".reset").on('click', function(){
-        $(".music").show();
-        reboot.play();
-        games_played++;
-        reset_stats();
-        display_stats();
-        first_card_clicked = null;
-        second_card_clicked = null;
-        $(".card").find(".back").html("<img src='images/skynet.jpg'>").fadeIn();
-        $(".title").html(game_title);
-        console.log("reboot");
     });
 });
