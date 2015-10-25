e478140e4df07f3beb0302688dcc4f5e79c6519c
Chelsea <chelseadeonne@gmail.com>
2015-10-25 13:06:23 -0700

    added effects to game_title and cards when matches get triggered

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ var accuracy = 0;
 var games_played = 0;
 var music_theme = true;
 var winner = $("<h1 class='winner'>Winner!</h1>");
-var game_title = $("<h1>Terminator Match Game</h1>");
+var game_title = $("<h1 class='title'><span id='t1'>T</span><span id='e'>e</span><span id='r1'>r</span><span id='m'>m</span><span id='i'>i</span><span id='n'>n</span><span id='a'>a</span><span id='t2'>t</span><span id='o'>o</span><span id='r2'>r</span><span class='red'> Match Game</span></h1>");
+//var game_title = $("<h1>Terminator Match Game</h1>");
 var again = $("<h2 class='again'>Play Again?</h2>");
 var yes = $("<button class='affirmative'>Affirmative</button>");
 var no = $("<button class='negative'>Negative</button>");
@@ -26,6 +27,7 @@ function calculate_accuracy() {
     }
 }
 function reset_stats() {
+    game_title = $("<h1 class='title'><span id='t1'>T</span><span id='e'>e</span><span id='r1'>r</span><span id='m'>m</span><span id='i'>i</span><span id='n'>n</span><span id='a'>a</span><span id='t2'>t</span><span id='o'>o</span><span id='r2'>r</span><span class='red'> Match Game</span></h1>");
     accuracy = 0;
     matches = 0;
     attempts = 0;
@@ -42,30 +44,31 @@ function you_won(){
     });
 }
 $(document).ready(function(){
-    var one = $("<div class='card' id='card1'><div class='front' id='front1' data-card='reese'><img src='images/t1-reese.png'></div><div class='back' id='back1'></div></div>");
-    var two = $("<div class='card' id='card2'><div class='front' id='front2' data-card='t1-sarah'><img src='images/t1-sarah.jpg'></div><div class='back' id='back2'></div></div>");
-    var three = $("<div class='card' id='card3'><div class='front' id='front3' data-card='t1-terminator'><img src='images/t1-terminator.jpg'></div><div class='back' id='back3'></div></div>");
-    var four = $("<div class='card' id='card4'><div class='front' id='front4' data-card='reese'><img src='images/t1-reese.png'></div><div class='back' id='back4'></div></div>");
-    var five = $("<div class='card' id='card5'><div class='front' id='front5' data-card='t2-arnold-john'><img src='images/t2-arnold-john.jpg'></div><div class='back' id='back5'></div></div>");
-    var six = $("<div class='card' id='card6'><div class='front' id='front6' data-card='t2-terminator'><img src='images/t2-terminator.jpg'></div><div class='back' id='back6'></div></div>");
-    var seven = $("<div class='card' id='card7'><div class='front' id='front7' data-card='t1-sarah'><img src='images/t1-sarah.jpg'></div><div class='back' id='back7'></div></div>");
-    var eight = $("<div class='card' id='card8'><div class='front' id='front8' data-card='t3-terminator'><img src='images/t3-terminator.jpg'></div><div class='back' id='back8'></div></div>");
-    var nine = $("<div class='card' id='card9'><div class='front' id='front9' data-card='t4-marcus'><img src='images/t4-marcus.jpg'></div><div class='back' id='back9'></div></div>");
-    var ten = $("<div class='card' id='card10'><div class='front' id='front10' data-card='t2-arnold-john'><img src='images/t2-arnold-john.jpg'></div><div class='back' id='back10'></div></div>");
-    var eleven = $("<div class='card' id='card11'><div class='front' id='front11' data-card='t5-john'><img src='images/t5-john.jpg'></div><div class='back' id='back1'></div></div>");
-    var twelve = $("<div class='card' id='card12'><div class='front' id='front12' data-card='t4-marcus'><img src='images/t4-marcus.jpg'></div><div class='back' id='back12'></div></div>");
-    var thirteen = $("<div class='card' id='card13'><div class='front' id='front13' data-card='t1-terminator'><img src='images/t1-terminator.jpg'></div><div class='back' id='back13'></div></div>");
-    var fourteen = $("<div class='card' id='card14'><div class='front' id='front14' data-card='t5-sarah-kyle'><img src='images/t5-sarah-kyle.jpg'></div><div class='back' id='back14'></div></div>");
-    var fifteen = $("<div class='card' id='card15'><div class='front' id='front15' data-card='t5-john'><img src='images/t5-john.jpg'></div><div class='back' id='back15'></div></div>");
-    var sixteen = $("<div class='card' id='card16'><div class='front' id='front16' data-card='t2-terminator'><img src='images/t2-terminator.jpg'></div><div class='back' id='back16'></div></div>");
-    var seventeen = $("<div class='card' id='card17'><div class='front' id='front17' data-card='t3-terminator'><img src='images/t3-terminator.jpg'></div><div class='back' id='back17'></div></div>");
-    var eighteen = $("<div class='card' id='card18'><div class='front' id='front18' data-card='t5-sarah-kyle'><img src='images/t5-sarah-kyle.jpg'></div><div class='back' id='back18'></div></div>");
+    var one = $("<div class='card' id='card1'><div class='front' id='front1' data-card='reese'><img src='images/t1-reese.png'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back1'></div></div>");
+    var two = $("<div class='card' id='card2'><div class='front' id='front2' data-card='t1-sarah'><img src='images/t1-sarah.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back2'></div></div>");
+    var three = $("<div class='card' id='card3'><div class='front' id='front3' data-card='t1-terminator'><img src='images/t1-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back3'></div></div>");
+    var four = $("<div class='card' id='card4'><div class='front' id='front4' data-card='reese'><img src='images/t1-reese.png'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back4'></div></div>");
+    var five = $("<div class='card' id='card5'><div class='front' id='front5' data-card='t2-arnold-john'><img src='images/t2-arnold-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back5'></div></div>");
+    var six = $("<div class='card' id='card6'><div class='front' id='front6' data-card='t2-terminator'><img src='images/t2-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back6'></div></div>");
+    var seven = $("<div class='card' id='card7'><div class='front' id='front7' data-card='t1-sarah'><img src='images/t1-sarah.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back7'></div></div>");
+    var eight = $("<div class='card' id='card8'><div class='front' id='front8' data-card='t3-terminator'><img src='images/t3-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back8'></div></div>");
+    var nine = $("<div class='card' id='card9'><div class='front' id='front9' data-card='t4-marcus'><img src='images/t4-marcus.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back9'></div></div>");
+    var ten = $("<div class='card' id='card10'><div class='front' id='front10' data-card='t2-arnold-john'><img src='images/t2-arnold-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back10'></div></div>");
+    var eleven = $("<div class='card' id='card11'><div class='front' id='front11' data-card='t5-john'><img src='images/t5-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back1'></div></div>");
+    var twelve = $("<div class='card' id='card12'><div class='front' id='front12' data-card='t4-marcus'><img src='images/t4-marcus.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back12'></div></div>");
+    var thirteen = $("<div class='card' id='card13'><div class='front' id='front13' data-card='t1-terminator'><img src='images/t1-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back13'></div></div>");
+    var fourteen = $("<div class='card' id='card14'><div class='front' id='front14' data-card='t5-sarah-kyle'><img src='images/t5-sarah-kyle.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back14'></div></div>");
+    var fifteen = $("<div class='card' id='card15'><div class='front' id='front15' data-card='t5-john'><img src='images/t5-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back15'></div></div>");
+    var sixteen = $("<div class='card' id='card16'><div class='front' id='front16' data-card='t2-terminator'><img src='images/t2-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back16'></div></div>");
+    var seventeen = $("<div class='card' id='card17'><div class='front' id='front17' data-card='t3-terminator'><img src='images/t3-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back17'></div></div>");
+    var eighteen = $("<div class='card' id='card18'><div class='front' id='front18' data-card='t5-sarah-kyle'><img src='images/t5-sarah-kyle.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back18'></div></div>");
     var all_cards = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen];
     all_cards.sort(function() {
         return 0.5 - Math.random()
     });
     theme_music.play();
     $("#game-area").append(all_cards);
+    $(".card").find(".front").find(".terminated").fadeOut('fast');
     $(".card").on('click', function(){
         card_flip.play();
         var current_card = $(this).find(".front").attr("data-card");
@@ -85,7 +88,11 @@ $(document).ready(function(){
             if (first_card_clicked === second_card_clicked) {
                 match.play();
                 console.log("match!");
+                $(card1).find('.front').find('.terminated').fadeIn(2000);
+                $(card2).find('.front').find('.terminated').fadeIn(2000);
                 $(card1).addClass('disabled');
+
+                //$(card1).addClass('match');
                 first_card_clicked = null;
                 second_card_clicked = null;
                 matches++;
@@ -96,6 +103,7 @@ $(document).ready(function(){
                             all_cards.sort(function() {
                                 return 0.5 - Math.random()
                             });
+                            $(".card").find(".front").find(".terminated").fadeOut();
                             $("#game-area").append(all_cards);
                             $(".music").show().html("Stop Music");
                             music_theme = true;
@@ -138,7 +146,40 @@ $(document).ready(function(){
             }
         }
         display_stats();
+        switch (matches) {
+            case 0:
+                $('.title').addClass('.title');
+                break;
+            case 1:
+                $('#e').addClass('melting_red');
+                break;
+            case 2:
+                $('#r1').addClass('melting_red');
+                break;
+            case 3:
+                $('#m').addClass('melting_red');
+                break;
+            case 4:
+                $('#i').addClass('melting_red');
+                break;
+            case 5:
+                $('#n').addClass('melting_red');
+                break;
+            case 6:
+                $('#a').addClass('melting_red');
+                break;
+            case 7:
+                $('#t2').addClass('melting_red');
+                break;
+            case 8:
+                $('#o').addClass('melting_red');
+                break;
+            case 9:
+                $('#r2').css('color', '#960b27');
+                break;
+        }
     });
+
     $(".music").on('click', function(){
         if (music_theme === true) {
             music_theme = false;
@@ -170,6 +211,7 @@ $(document).ready(function(){
         first_card_clicked = null;
         second_card_clicked = null;
         $(".card").removeClass('disabled').find(".back").html("<img src='images/skynet.jpg'>").fadeIn();
+        $(".card").find(".front").find(".terminated").fadeOut();
         $(".title").html(game_title);
         console.log("reboot");
     });
