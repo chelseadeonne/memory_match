2fed4e5d3c73b7bc91b3cd530a86b6ab3290a922
Chelsea <chelseadeonne@gmail.com>
2015-10-28 10:01:34 -0700

    cleaned up card variables, removing unnecessary id's. commented out all_cards.sort function on document load and placed matching cards next to each other to easily present all features efficiently - will uncomment that when necessary

--- script.js
+++ script.js
@@ -44,28 +44,28 @@ function you_won(){
     });
 }
 $(document).ready(function(){
-    var one = $("<div class='card' id='card1'><div class='front' id='front1' data-card='reese'><img src='images/t1-reese.png'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back1'></div></div>");
-    var two = $("<div class='card' id='card2'><div class='front' id='front2' data-card='t1-sarah'><img src='images/t1-sarah.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back2'></div></div>");
-    var three = $("<div class='card' id='card3'><div class='front' id='front3' data-card='t1-terminator'><img src='images/t1-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back3'></div></div>");
-    var four = $("<div class='card' id='card4'><div class='front' id='front4' data-card='reese'><img src='images/t1-reese.png'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back4'></div></div>");
-    var five = $("<div class='card' id='card5'><div class='front' id='front5' data-card='t2-arnold-john'><img src='images/t2-arnold-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back5'></div></div>");
-    var six = $("<div class='card' id='card6'><div class='front' id='front6' data-card='t2-terminator'><img src='images/t2-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back6'></div></div>");
-    var seven = $("<div class='card' id='card7'><div class='front' id='front7' data-card='t1-sarah'><img src='images/t1-sarah.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back7'></div></div>");
-    var eight = $("<div class='card' id='card8'><div class='front' id='front8' data-card='t3-terminator'><img src='images/t3-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back8'></div></div>");
-    var nine = $("<div class='card' id='card9'><div class='front' id='front9' data-card='t4-marcus'><img src='images/t4-marcus.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back9'></div></div>");
-    var ten = $("<div class='card' id='card10'><div class='front' id='front10' data-card='t2-arnold-john'><img src='images/t2-arnold-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back10'></div></div>");
-    var eleven = $("<div class='card' id='card11'><div class='front' id='front11' data-card='t5-john'><img src='images/t5-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back1'></div></div>");
-    var twelve = $("<div class='card' id='card12'><div class='front' id='front12' data-card='t4-marcus'><img src='images/t4-marcus.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back12'></div></div>");
-    var thirteen = $("<div class='card' id='card13'><div class='front' id='front13' data-card='t1-terminator'><img src='images/t1-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back13'></div></div>");
-    var fourteen = $("<div class='card' id='card14'><div class='front' id='front14' data-card='t5-sarah-kyle'><img src='images/t5-sarah-kyle.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back14'></div></div>");
-    var fifteen = $("<div class='card' id='card15'><div class='front' id='front15' data-card='t5-john'><img src='images/t5-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back15'></div></div>");
-    var sixteen = $("<div class='card' id='card16'><div class='front' id='front16' data-card='t2-terminator'><img src='images/t2-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back16'></div></div>");
-    var seventeen = $("<div class='card' id='card17'><div class='front' id='front17' data-card='t3-terminator'><img src='images/t3-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back17'></div></div>");
-    var eighteen = $("<div class='card' id='card18'><div class='front' id='front18' data-card='t5-sarah-kyle'><img src='images/t5-sarah-kyle.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back' id='back18'></div></div>");
-    var all_cards = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen];
+    var one = $("<div class='card'><div class='front' data-card='reese'><img src='images/t1-reese.png'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var two = $("<div class='card'><div class='front' data-card='reese'><img src='images/t1-reese.png'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var three = $("<div class='card'><div class='front' data-card='t1-terminator'><img src='images/t1-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var four = $("<div class='card'><div class='front' data-card='t1-terminator'><img src='images/t1-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var five = $("<div class='card'><div class='front' data-card='t2-arnold-john'><img src='images/t2-arnold-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var six = $("<div class='card'><div class='front' data-card='t2-arnold-john'><img src='images/t2-arnold-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var seven = $("<div class='card'><div class='front' data-card='t2-terminator'><img src='images/t2-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var eight = $("<div class='card'><div class='front'data-card='t2-terminator'><img src='images/t2-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var nine = $("<div class='card'><div class='front'data-card='t1-sarah'><img src='images/t1-sarah.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var ten = $("<div class='card'><div class='front'data-card='t1-sarah'><img src='images/t1-sarah.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var eleven = $("<div class='card'><div class='front'data-card='t3-terminator'><img src='images/t3-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var twelve = $("<div class='card'><div class='front' data-card='t3-terminator'><img src='images/t3-terminator.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var thirteen = $("<div class='card'><div class='front' data-card='t4-marcus'><img src='images/t4-marcus.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var fourteen = $("<div class='card'><div class='front' data-card='t4-marcus'><img src='images/t4-marcus.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var fifteen = $("<div class='card'><div class='front' data-card='t5-john'><img src='images/t5-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var sixteen = $("<div class='card'><div class='front' data-card='t5-john'><img src='images/t5-john.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var seventeen = $("<div class='card'><div class='front'data-card='t5-sarah-kyle'><img src='images/t5-sarah-kyle.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var eighteen = $("<div class='card'><div class='front' data-card='t5-sarah-kyle'><img src='images/t5-sarah-kyle.jpg'><img class='terminated'src='images/terminated.png'></div><div class='back'></div></div>");
+    var all_cards = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen];  /*
     all_cards.sort(function() {
         return 0.5 - Math.random()
-    });
+    }); */
     theme_music.play();
     $("#game-area").append(all_cards);
     $(".card").find(".front").find(".terminated").fadeOut('fast');
